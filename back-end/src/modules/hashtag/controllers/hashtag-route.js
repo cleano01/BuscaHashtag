f4eee19ec6d857dbab6e8dec45ee512fd8dcf47b
cleano01 <cleano.ferreira@dce.ufpb.br>
2020-02-19 21:30:19 -0300

    (tabela) ajustando tempo e break de resposta no back-end para recebimento de respota no front-end

--- back-end/src/modules/hashtag/controllers/hashtag-route.js
+++ back-end/src/modules/hashtag/controllers/hashtag-route.js
@@ -4,14 +4,18 @@ import {lista_tweets, parameters, stream } from '../service/service'
 const hashtagRouter = express.Router();
 
 hashtagRouter.route('/parar').get((req, res) => {  
-  console.log(parameters)
   res.send(lista_tweets);
 });
 
-hashtagRouter.route('/buscar').get( (req, res)  =>{
+hashtagRouter.route('/buscar').get(  async (req, res)  =>{
   parameters.track="#RaspemOSovaco,#bbb,#BBB"
-  stream()    
-  res.send({'Status':'Busca feita com sucesso'}) 
+  console.log('calculando')
+  stream()
+  setTimeout(()=>{
+
+    res.send(lista_tweets) 
+  }, 15000);
+   console.log('ok')
 });
 
 export default hashtagRouter;
--- back-end/src/modules/hashtag/service/service.js
+++ back-end/src/modules/hashtag/service/service.js
@@ -3,9 +3,8 @@ import twitter_config from '../../../config/twitter-config'
 export var lista_tweets = [];
 
 export const parameters = {
-  track: "#RaspemOSovaco",        
+  track: "#RaspemOSovaco,#bbb,#BBB",        
 };
-
 export const  stream = () => {
   twitter_config.stream('statuses/filter', 
   parameters, (stream) => {
@@ -22,4 +21,40 @@ export const  stream = () => {
     setTimeout(stream.destroy, 10000)   
   })
 }
+/*
+export  let  stream =  () => {
+    twitter_config.stream('statuses/filter', 
+    parameters, (stream) => {
+      //console.log(parameters)
+      //console.log('----------------')
+      console.log('aqui')
+
+      stream.on('data',  (tweet) =>{
+        
+          lista_tweets.push(tweet);      
+          cont++;     
+          console.log("$$$$$ ", cont)
+          console.log(tweet)  
+          
+ 
+        
+        cont++
+        if(cont == 3){
+          console.log('############### 3')
+         }
+               
+      });
+  
+      stream.on('error', (error) => {
+        console.log(error)
+      });
+      //setmeout(stream.destroy, 6000);  
+    } )
+
+  stream = undefined;
+
+  return lista_tweets
+
+}
+*/
 
